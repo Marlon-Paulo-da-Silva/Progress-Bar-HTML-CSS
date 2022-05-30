7f4b3e9fe63680949c516bc8bd197e9410f5e5a7
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-05-30 13:40:42 -0300

    Colocando os conteúdos

--- barra-progressiva-listar.php
+++ barra-progressiva-listar.php
@@ -4,29 +4,55 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   <title>Barra Progressiva</title>
 </head>
 <body>
   <div class="container">
     <input type="radio" name="profile" id="profile" class="profile" checked >
-    <input type="radio" name="profile" id="settings" class="settings" >
+    <input type="radio" name="settings" id="settings" class="settings" >
     <input type="radio" name="post" id="post"  >
     <input type="radio" name="books" id="books"  >
     <div class="head">
       <ul class="nav">
         <li class="st stl">
-          <h2 class="inner">Profile</h2>
+          <h2 class="inner">Perfil</h2>
         </li>
-        <li class="st stl">
-          <h2 class="inner">Profile</h2>
+        <li class="st st2">
+          <h2 class="inner">Conta</h2>
         </li>
-        <li class="st stl">
-          <h2 class="inner">Profile</h2>
+        <li class="st st3">
+          <h2 class="inner">Publicação</h2>
         </li>
-        <li class="st stl">
-          <h2 class="inner">Profile</h2>
+        <li class="st st4">
+          <h2 class="inner">Avançado</h2>
         </li>
       </ul>
+      <div class="line">
+        <span></span>
+      </div>
+    </div>
+    <div class="content">
+      <section class="profile-section">
+        <span><i class="fa fa-house"></i></span>
+        <h1>perfil Section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt sequi dolore delectus nostrum eius, fugiat, provident numquam quia totam alias ab. Obcaecati saepe provident ratione aspernatur tempore ipsum impedit reiciendis!</p>
+      </section>
+      <section class="account-section">
+        <span><i class="fa fa-user"></i></span>
+        <h1>Conta Section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt sequi dolore delectus nostrum eius, fugiat, provident numquam quia totam alias ab. Obcaecati saepe provident ratione aspernatur tempore ipsum impedit reiciendis!</p>
+      </section>
+      <section class="post-section">
+        <span><i class="fa fa-edit"></i></span>
+        <h1>Publicação Section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt sequi dolore delectus nostrum eius, fugiat, provident numquam quia totam alias ab. Obcaecati saepe provident ratione aspernatur tempore ipsum impedit reiciendis!</p>
+      </section>
+      <section class="advanced-section">
+        <span><i class="fa fa-cog"></i></span>
+        <h1>Avançado Section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt sequi dolore delectus nostrum eius, fugiat, provident numquam quia totam alias ab. Obcaecati saepe provident ratione aspernatur tempore ipsum impedit reiciendis!</p>
+      </section>
     </div>
   </div>
 </body>
