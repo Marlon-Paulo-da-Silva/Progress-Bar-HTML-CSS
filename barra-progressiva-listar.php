e45edacdebc80e9ecb576788ff257cb459a4d275
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-05-30 13:01:09 -0300

    first commit

---
+++ barra-progressiva-listar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Barra Progressiva</title>
+</head>
+<body>
+  <div class="container">
+    <input type="radio" name="profile" id="profile" class="profile" checked >
+    <input type="radio" name="profile" id="settings" class="settings" >
+    <input type="radio" name="post" id="post"  >
+    <input type="radio" name="books" id="books"  >
+    <div class="head">
+      <ul class="nav">
+        <li class="st stl">
+          <h2 class="inner">Profile</h2>
+        </li>
+        <li class="st stl">
+          <h2 class="inner">Profile</h2>
+        </li>
+        <li class="st stl">
+          <h2 class="inner">Profile</h2>
+        </li>
+        <li class="st stl">
+          <h2 class="inner">Profile</h2>
+        </li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
