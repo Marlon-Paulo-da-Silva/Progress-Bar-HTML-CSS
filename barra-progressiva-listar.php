594da7ec9a8a665043c733000303975839d77a0b
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-05-30 15:17:37 -0300

    estilizando os passos, colocando a barra

--- barra-progressiva-listar.php
+++ barra-progressiva-listar.php
@@ -4,8 +4,124 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   <title>Barra Progressiva</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <style>
+    * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+    body {
+      font-family: tahoma;
+      overflow: hidden;
+    }
+    .container {
+      margin: 150px auto;
+      width: 80vw;
+    }
+    input {
+      display: none;
+      position: absolute;
+    }
+    .head {
+      position: relative;
+      width: 90%;
+      margin: auto;
+      height: 100px;
+      margin-top: 60px;
+      animation: trans .7s linear forwards;
+    }
+    ul.nav {
+      padding-top: 15px;
+      z-index: 100;
+      position: absolute;
+      width: 100%;
+      margin-top: -40px;
+    }
+    ul.nav li {
+      list-style: none;
+      float: left;
+      width: 25%;
+      text-align: center;
+      color: #aaa;
+      text-transform: uppercase;
+      font-size: 11px;
+      cursor: pointer;
+      font-weight: 700;
+      transition: all .3s ease;
+      vertical-align: middle;
+      height: 60px;
+      position: relative;
+
+    }
+
+    ul.nav li::before {
+      content: '';
+      width: 25px;
+      height: 25px;
+      border: 2px solid #aaa;
+      border-radius: 50%;
+      margin: auto;
+      display: block;
+      transition: all .3s ease;
+    }
+    ul.nav li:hover::before{
+      border: 2px solid #fff;
+      background: #777;
+    }
+    ul.nav li .inner{
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+      font-size: 14px;
+      font-weight: bolder;
+      
+    }
+    ul.nav li.active,
+    ul.nav li:hover{
+      color: #444;
+    }
+    ul.nav li::after {
+      content: '';
+      display: block;
+      width: 7px;
+      height: 7px;
+      background: #777;
+      border-radius: 50%;
+      margin: auto;
+      border: 7px solid #fff;
+      margin-top: 40px;
+      box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    ul.nav li:hover::after,
+    ul.nav li.active::after{
+      background: #2CA62F;
+    }
+    .line {
+        width: 76%;
+        margin: auto;
+        background: #eee;
+        height: 7px;
+        position: absolute;
+        left: 11.5%;
+        top: 52px;
+        z-index: 1;
+        border-radius: 50px;
+    }
+    .line span {
+      content: '';
+      width: 50%;
+      height: 100%;
+      background-image: linear-gradient(to right, #2CA62F 0%, #F8C300 100%);
+      position: absolute;
+      z-index: 2;
+      border-radius: 50px;
+      transition: all .8s ease;
+    }
+
+
+  </style>
 </head>
 <body>
   <div class="container">
@@ -15,7 +131,7 @@
     <input type="radio" name="books" id="books"  >
     <div class="head">
       <ul class="nav">
-        <li class="st stl">
+        <li class="st stl active">
           <h2 class="inner">Perfil</h2>
         </li>
         <li class="st st2">
